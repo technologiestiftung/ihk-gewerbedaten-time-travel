1d2d32293a5c57a237028b946e692a780e9b0eb6
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-07-26 14:27:29 +0200

    style: use correct basemap

--- js/utils/map-utils.js
+++ js/utils/map-utils.js
@@ -1,7 +1,7 @@
-/** Restrcited to only work from specific dev machines and the deployment */
-export const MAPTILER_KEY = "vqDzDaeBTg5TOgNrkUmV";
+/** Restricted to only work from specific dev machines and the deployment */
+export const MAPTILER_KEY = "OQMCAqaBYGzsY2czeHep";
 
-export const MAPTILER_BASEMAP_STYLE_JSON_URL = `https://api.maptiler.com/maps/5c809f85-a2fd-4285-bf88-6cd963431ba1/style.json?key=${MAPTILER_KEY}`;
+export const MAPTILER_BASEMAP_STYLE_JSON_URL = `https://api.maptiler.com/maps/164a48b8-1989-4f18-8469-6a61366b73e6/style.json?key=${MAPTILER_KEY}`;
 
 export const BUSINESSES_VECTOR_TILESET_URL =
   "https://ihk-gewerbedaten-pmtiles.s3.eu-central-1.amazonaws.com/data.pmtiles";
